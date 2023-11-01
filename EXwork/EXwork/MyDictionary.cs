95de1fe7e8ddfe6062097b6f2f6800ab14dc594a
xzWhat <115475302+coffeedipso@users.noreply.github.com>
2023-11-01 18:43:19 +0300

    Add files via upload

---
+++ EXwork/EXwork/MyDictionary.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Exam
+{
+    internal class MyDictionary
+    {
+        public static Func<string, bool> IsRussianLettersOnly = r => Regex.IsMatch(r, "^[А-Яа-я,]+$");
+        public static Func<string, bool> IsEnglLettersOnly = e => Regex.IsMatch(e, "^[A-Za-z,]+$");
+        public static string GenerateWordEngl(int counter)
+        {
+            string result = Regex.Replace("a", @"[a-zA-Z]", m => ((char)(m.Value[0] + counter - 1)).ToString().ToUpper());
+            return result;
+        }
+        public static string GenerateWordRus(int counter)
+        {
+            string result = Regex.Replace("а", @"[А-Яа-я]", m => ((char)(m.Value[0] + counter - 1)).ToString().ToUpper());
+            return result;
+        }
+
+        public static void ShowDictionary(Dictionary<string, string> EnglRusDictionary)
+        {
+            foreach (var word in EnglRusDictionary)
+            {
+                Console.WriteLine($"key: {word.Key}  value: {word.Value}");
+            }
+        }
+        public static Dictionary<string, string> AddWordAndTranslation(Dictionary<string, string> EnglRusDictionary, string word, string translation)
+        {
+            EnglRusDictionary[word] = translation;
+            return EnglRusDictionary;
+        }
+        public static Dictionary<string, string> ChangeTranslation(Dictionary<string, string> EnglRusDictionary, string Keys, string newWord)
+        {
+            EnglRusDictionary[Keys] = newWord;
+            return EnglRusDictionary;
+        }
+        public static Dictionary<string, string> ChangeWord(Dictionary<string, string> EnglRusDictionary, string Keys, string newWord)
+        {
+            var value = EnglRusDictionary[Keys];
+            EnglRusDictionary.Remove(Keys);
+            EnglRusDictionary.Add(newWord, value);
+            return EnglRusDictionary;
+        }
+        public static void FindWordFromDictionary(Dictionary<string, string> EnglRusDictionary, string wordForTranslate)
+        {
+            var translation = EnglRusDictionary.FirstOrDefault(x => x.Key == wordForTranslate);
+            if (translation.Key != null)
+            {
+                Console.WriteLine($"Перевод слова {translation.Key} - {translation.Value}");
+            }
+        }
+        public static Dictionary<string, string> DeleteWord(Dictionary<string, string> EnglRusDictionary, string DeleteWord, string typeOfDictionary, int counter)
+        {
+            string EnglWord = $"none{GenerateWordEngl(counter)}";
+            string RusWord = $"Пусто{GenerateWordRus(counter)}";
+            var value = EnglRusDictionary[DeleteWord];
+            EnglRusDictionary.Remove(DeleteWord);
+            EnglRusDictionary.Add(typeOfDictionary == "Rus-Engl" ? RusWord : EnglWord, value);
+            return EnglRusDictionary;
+        }
+        public static Dictionary<string, string> DeleteTranslation(Dictionary<string, string> EnglRusDictionary, string TranslationDelete, string typeOfDictionary, int counter)
+        {
+            string EnglWord = $"none{GenerateWordEngl(counter)}";
+            string RusWord = $"Пусто{GenerateWordRus(counter)}";
+            var value = EnglRusDictionary[TranslationDelete];
+            EnglRusDictionary.Remove(TranslationDelete);
+            EnglRusDictionary.Add(TranslationDelete, typeOfDictionary == "Rus-Engl" ? EnglWord : RusWord);
+            return EnglRusDictionary;
+        }
+        public static void GetFile(Dictionary<string, string> EnglRusDictionary)
+        {
+            using (StreamWriter writer = new StreamWriter("словарь.txt"))
+            {
+
+                foreach (var pair in EnglRusDictionary)
+                {
+                    writer.WriteLine($"{pair.Key}: {pair.Value}");
+                }
+            }
+        }
+        public static Dictionary<string, string> AddAdditionalTranslation(Dictionary<string, string> EnglRusDictionary, string additionalTranslation, string translationForWord)
+        {
+            EnglRusDictionary[additionalTranslation] += "," + translationForWord;
+            return EnglRusDictionary;
+        }
+    }
+}
---
+++ EXwork/EXwork/Program.cs
@@ -0,0 +1,212 @@
+﻿using System.Text.RegularExpressions;
+using Exam;
+
+int choice = 0;
+string typeOfDictionary = "";
+int menu()
+{
+    Console.WriteLine("\n" +
+        //   "\n Создать словарь (Engl->Rus) - 1 " +
+        //     "\n Создать словарь (Rus->Engl) - 2 " +
+        " Добавить слово и его перевод - 1 " +
+        "\n Заменить перевод - 2 " +
+        "\n Заменить слово - 3" +
+        "\n Искать перевод - 4" +
+        "\n Удалить слово - 5" +
+        "\n Удалить перевод - 6" +
+        "\n Записать словарь в файл - 7" +
+        "\n Добавить доп.перевод - 8" +
+        "\n Выход - 9 ");
+    Console.WriteLine();
+    Console.WriteLine("Введите значение 1-8: ");
+    try
+    {
+        choice = Int32.Parse(Console.ReadLine());
+        if (choice < 1 || choice > 9)
+        {
+            Console.WriteLine("Некорректный ввод, введите еще раз:");
+            return menu();
+        }
+        return choice;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Некорректный ввод, введите еще раз:");
+        return menu();
+    }
+}
+int counter = 0;
+Dictionary<string, string> EnglRusDictionary = new Dictionary<string, string>();
+while (true)
+{
+    int ChoiceForSwitch = menu();
+    switch (ChoiceForSwitch)
+    {
+        case 1:
+
+            Console.WriteLine("Введите слово:");
+            string word = Console.ReadLine();
+            Console.WriteLine("Введите перевод:");
+            string translation = Console.ReadLine();
+            void ExeptFunction()
+            {
+                Console.WriteLine($"\nВаш словарь {typeOfDictionary}");
+                try
+                {
+                    counter++;
+                    EnglRusDictionary = MyDictionary.AddWordAndTranslation(EnglRusDictionary, word, translation);
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Ошибка при добавлении слова и перевода: " + ex.Message);
+                }
+            }
+            if (MyDictionary.IsEnglLettersOnly(word) && MyDictionary.IsRussianLettersOnly(translation) || MyDictionary.IsEnglLettersOnly(translation) && MyDictionary.IsRussianLettersOnly(word))
+            {
+
+                if (!EnglRusDictionary.Any())
+                {
+                    typeOfDictionary = MyDictionary.IsEnglLettersOnly(word) && MyDictionary.IsRussianLettersOnly(translation) ? "Engl-Rus" : "Rus-Engl";
+                    ExeptFunction();
+                }
+                else
+                {
+                    bool isFirstKeyEnglLettersOnly = MyDictionary.IsEnglLettersOnly(EnglRusDictionary.First().Key);
+                    bool isFirstValueRussianLettersOnly = MyDictionary.IsRussianLettersOnly(EnglRusDictionary.First().Value);
+
+                    if ((isFirstKeyEnglLettersOnly && isFirstValueRussianLettersOnly) && (MyDictionary.IsEnglLettersOnly(word) && MyDictionary.IsRussianLettersOnly(translation)))
+                    {
+                        typeOfDictionary = "Engl-Rus";
+                        ExeptFunction();
+                    }
+                    else if ((!isFirstKeyEnglLettersOnly && !isFirstValueRussianLettersOnly) && (MyDictionary.IsRussianLettersOnly(word) && MyDictionary.IsEnglLettersOnly(translation)))
+                    {
+                        typeOfDictionary = "Rus-Engl";
+                        ExeptFunction();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nНекорректный ввод. Ваш словарь {typeOfDictionary}");
+                    }
+                }
+
+                Console.WriteLine();
+                MyDictionary.ShowDictionary(EnglRusDictionary);
+            }
+            else
+            {
+                Console.WriteLine("\nНекорректный ввод");
+            }
+            break;
+        case 2:
+            Console.WriteLine("Введите слово(ключ),перевод которого хотите заменить: ");
+            string oldWord = Console.ReadLine();
+            Console.WriteLine("Введите новый перевод(значение), на который заменяете старый: ");
+            string newWord = Console.ReadLine();
+
+            if (EnglRusDictionary.ContainsKey(oldWord) && newWord is string)
+            {
+                if (MyDictionary.IsRussianLettersOnly(newWord) && MyDictionary.IsEnglLettersOnly(oldWord) || MyDictionary.IsRussianLettersOnly(oldWord) && MyDictionary.IsEnglLettersOnly(newWord))
+                {
+                    EnglRusDictionary = MyDictionary.ChangeTranslation(EnglRusDictionary, oldWord, newWord);
+                    Console.WriteLine();
+                    MyDictionary.ShowDictionary(EnglRusDictionary);
+                }
+                else
+                {
+                    Console.WriteLine($"Ваш словарь {typeOfDictionary}, некорректный ввод");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Слово в словаре отсутствует/некорректный ввод");
+            }
+            break;
+        case 3:
+            Console.WriteLine("Введите слово(ключ), которое хотите заменить: ");
+            string oldWord2 = Console.ReadLine();
+            Console.WriteLine("Введите новое слово, на которое заменяете старое: ");
+            string newWord2 = Console.ReadLine();
+            if (EnglRusDictionary.ContainsKey(oldWord2) && newWord2 is string)
+            {
+                if (MyDictionary.IsRussianLettersOnly(newWord2) && MyDictionary.IsRussianLettersOnly(oldWord2) || MyDictionary.IsEnglLettersOnly(oldWord2) && MyDictionary.IsEnglLettersOnly(newWord2))
+                {
+                    EnglRusDictionary = MyDictionary.ChangeWord(EnglRusDictionary, oldWord2, newWord2);
+                    Console.WriteLine();
+                    MyDictionary.ShowDictionary(EnglRusDictionary);
+                }
+                else
+                {
+                    Console.WriteLine($"Ваш словарь {typeOfDictionary}, некорректный ввод");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Слово в словаре отсутствует/некорректный ввод");
+            }
+            break;
+        case 4:
+            Console.WriteLine("Напоминание:" + $"Ваш словарь {typeOfDictionary}");
+            Console.WriteLine("Введите слово, перевод которого хотите узнать: ");
+            string wordForTranslate = Console.ReadLine();
+            if (EnglRusDictionary.ContainsKey(wordForTranslate) && wordForTranslate is string)
+            {
+                MyDictionary.FindWordFromDictionary(EnglRusDictionary, wordForTranslate);
+            }
+            else
+            {
+                Console.WriteLine("Слово в словаре отсутствует/некорректный ввод");
+            }
+            break;
+        case 5:
+            Console.WriteLine("Введите слово, которое хотите удалить: ");
+            string wordDelete = Console.ReadLine();
+            if (EnglRusDictionary.ContainsKey(wordDelete) && wordDelete is string)
+            {
+                EnglRusDictionary = MyDictionary.DeleteWord(EnglRusDictionary, wordDelete, typeOfDictionary, counter);
+                Console.WriteLine();
+                MyDictionary.ShowDictionary(EnglRusDictionary);
+            }
+            else
+            {
+                Console.WriteLine("Слово в словаре отсутствует/некорректный ввод");
+            }
+            break;
+        case 6:
+            Console.WriteLine("Введите слово, перевод которого хотите удалить: ");
+            string TranslationDelete = Console.ReadLine();
+            if (EnglRusDictionary.ContainsKey(TranslationDelete) && TranslationDelete is string)
+            {
+                EnglRusDictionary = MyDictionary.DeleteTranslation(EnglRusDictionary, TranslationDelete, typeOfDictionary, counter);
+                Console.WriteLine();
+                MyDictionary.ShowDictionary(EnglRusDictionary);
+            }
+            else
+            {
+                Console.WriteLine("Слово в словаре отсутствует/некорректный ввод");
+            }
+            break;
+        case 7:
+            MyDictionary.GetFile(EnglRusDictionary);
+            Console.WriteLine("Текстовый файл получен");
+            break;
+        case 8:
+            Console.WriteLine("Введите слово, которому хотите добавить перевод: ");
+            string additionalTranslation = Console.ReadLine();
+            Console.WriteLine("Введите перевод:");
+            string translationForWord = Console.ReadLine();
+            if (EnglRusDictionary.ContainsKey(additionalTranslation) && additionalTranslation is string)
+            {
+                EnglRusDictionary = MyDictionary.AddAdditionalTranslation(EnglRusDictionary, additionalTranslation, translationForWord);
+                Console.WriteLine();
+                MyDictionary.ShowDictionary(EnglRusDictionary);
+            }
+            else
+            {
+                Console.WriteLine("Слово в словаре отсутствует/некорректный ввод");
+            }
+            break;
+        case 9:
+            return;
+    }
+}
